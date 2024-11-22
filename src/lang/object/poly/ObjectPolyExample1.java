3cf0190bf70ba597b446cc0d030b36dd9319611a
dayday <digetlyn@naver.com>
2024-11-22 14:25:22 +0900

    equals

---
+++ src/lang/object/poly/ObjectPolyExample1.java
@@ -0,0 +1,15 @@
+package lang.object.poly;
+
+public class ObjectPolyExample1 {
+	public static void main(String[] args) {
+		Car car = new Car();
+		Dog dog = new Dog();
+
+		action(car);
+		action(dog);
+	}
+
+	private static void action(Object object) {
+
+	}
+}

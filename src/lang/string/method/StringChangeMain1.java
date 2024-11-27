cf99d2a0740fca741fd2a444a1135d822d04ae42
dayday <digetlyn@naver.com>
2024-11-27 12:20:02 +0900

    StringChangeMain1

---
+++ src/lang/string/method/StringChangeMain1.java
@@ -0,0 +1,13 @@
+package lang.string.method;
+
+public class StringChangeMain1 {
+
+	public static void main(String[] args) {
+
+		String str = "Hello, Java! Welcome to Java";
+
+		System.out.println("인덱스 7부터의 부분 문자열 : " + str.substring(7));
+		System.out.println("인덱스 7부터의 12까지 부분 문자열 : " + str.substring(7,12));
+
+	}
+}

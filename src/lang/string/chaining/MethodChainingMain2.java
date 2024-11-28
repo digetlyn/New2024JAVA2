53f3f8c35c73cdbdf74202503fd6e93c2e5282ca
dayday <digetlyn@naver.com>
2024-11-28 12:18:47 +0900

    TestString

--- src/lang/string/chaining/MethodChainingMain2.java
+++ src/lang/string/chaining/MethodChainingMain2.java
@@ -5,11 +5,12 @@ public class MethodChainingMain2 {
 	public static void main(String[] args) {
 
 		ValueAdder adder = new ValueAdder();
-		adder.add(1);
-		adder.add(2);
-		adder.add(3);
+		ValueAdder adder1 = adder.add(1);
+		ValueAdder adder2 = adder.add(2);
+		ValueAdder adder3 = adder.add(3);
 
-		int result = adder.getValue();
+
+		int result = adder3.getValue();
 		System.out.println("result = " + result);
 	}
 }

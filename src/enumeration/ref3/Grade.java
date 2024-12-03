4c243f45ced587b1b61268cbe6e049165c76ddf3
dayday <digetlyn@naver.com>
2024-12-03 15:23:09 +0900

    ref3

---
+++ src/enumeration/ref3/Grade.java
@@ -0,0 +1,21 @@
+package enumeration.ref3;
+
+public enum Grade {
+
+	BASIC(10), GOLD(20), DIAMOND(30);
+
+	private final int discountPercent;
+
+	Grade(int discountPercent) {
+		this.discountPercent = discountPercent;
+	}
+
+	public int getDiscountPercent() {
+		return discountPercent;
+	}
+
+	//추가  Grade내부에 discount()메서드를 만들어서 할인율을 스스로 계산한다.
+	public int discount(int price) {
+		return price * discountPercent /100;
+	}
+}

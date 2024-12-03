9415280adb2fb188f3b76f053fb5d129ed190759
dayday <digetlyn@naver.com>
2024-12-03 10:41:05 +0900

    열거형

---
+++ src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,19 @@
+package enumeration.ex3;
+
+
+
+public class ClassGradeEx3_2 {
+
+	public static void main(String[] args) {
+		int prrice = 10000;
+
+		DiscountService discountService = new DiscountService();
+
+/*
+		//Grade grade = new Grade();  //enum 생성 불가 enum은 이미 final class이기 때문에
+		int result = discountService.discount(Grade, prrice);
+		System.out.println("NewClassGrade 등급의 할인 금액 : " + result);
+*/
+
+	}
+}

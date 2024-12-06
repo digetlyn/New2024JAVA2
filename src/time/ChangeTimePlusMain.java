b9a793f32b81b18df962525362a601a509f9bbc3
dayday <digetlyn@naver.com>
2024-12-06 12:19:09 +0900

    FormattingMain

--- src/time/ChangeTimePlusMain.java
+++ src/time/ChangeTimePlusMain.java
@@ -1,6 +1,5 @@
 package time;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
---
+++ src/time/FormattingMain2.java
@@ -0,0 +1,7 @@
+package time;
+
+public class FormattingMain2 {
+	public static void main(String[] args) {
+
+	}
+}

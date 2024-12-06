4970f84f6311d4f062d3bacf46bc266fc0bb1840
dayday <digetlyn@naver.com>
2024-12-06 17:08:00 +0900

    TestBetween

---
+++ src/time/test/TestBetween.java
@@ -0,0 +1,23 @@
+package time.test;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class TestBetween {
+	public static void main(String[] args) {
+		LocalDate startDate = LocalDate.of(2024, 1, 1);
+		LocalDate endDate = LocalDate.of(2024, 11, 21);
+
+		Period period = Period.between(startDate, endDate);
+		long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+
+		System.out.println("시작날짜 : " + startDate);
+		System.out.println("목표날짜 : " + endDate);
+
+		System.out.println("남은기간 : " + period.getYears() + "년 " + period.getMonths() + "개월 " + period.getDays() + "일" );
+
+		System.out.println("D-day " + daysBetween + "일 남았습니다.");
+
+	}
+}

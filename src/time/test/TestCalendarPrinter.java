1caa31a73998e1ec0cdd1107c6ec6a7510cbb3b9
dayday <digetlyn@naver.com>
2025-03-06 00:01:19 +0900

    test1

---
+++ src/time/test/TestCalendarPrinter.java
@@ -0,0 +1,37 @@
+package time.test;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TestCalendarPrinter {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("년도를 입력하세요: ");
+		int year = scanner.nextInt();
+
+		System.out.print("월을 입력하세요: ");
+		int month = scanner.nextInt();
+
+		printCalender(year, month);
+
+
+
+	}
+
+	private static void printCalender(int year, int month) {
+
+		LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+		LocalDate firstDayOfNexMonth = firstDayOfMonth.plusMonths(1);
+
+
+		//월요일 = 1(1%7=1) ... 일요일 7 (7%7=0)
+		int offsetWeekDays = firstDayOfMonth.getDayOfWeek().getValue() % 7;
+
+		System.out.println("Su Mo Tu We Th Fr Sa ");
+
+
+	}
+}
